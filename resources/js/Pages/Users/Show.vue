0ab76a9a36261b3f67e967c216e0f4314263cafd
Evren Ceyhan <evren@ceyhan.io>
2023-02-25 14:04:08 +0100

    common crud layout impl.

--- resources/js/Pages/Users/Show.vue
+++ resources/js/Pages/Users/Show.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { Head, Link } from "@inertiajs/vue3";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Link } from "@inertiajs/vue3";
+import AuthenticatedCrudLayout from "@/Layouts/AuthenticatedCrudLayout.vue";
 
 defineProps({
     user: Object,
@@ -8,33 +8,14 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Dashboard" />
-
-    <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
-            >
-                {{ user.name }}
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div
-                    class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
-                >
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <div
-                            v-for="(value, key) in user"
-                            :key="key"
-                            class="mb-4"
-                        >
-                            <strong>{{ key }}:</strong> {{ value }}
-                        </div>
-                    </div>
+    <AuthenticatedCrudLayout :title="user.name">
+        <div class="divide-y divide-slate-200">
+            <div v-for="(value, key) in user" :key="key" class="flex p-4">
+                <div class="w-full overflow-hidden sm:flex gap-x-4">
+                    <p class="w-1/5 text-slate-900">{{ key }}</p>
+                    <p class="text-slate-500 truncate">{{ value }}</p>
                 </div>
             </div>
         </div>
-    </AuthenticatedLayout>
+    </AuthenticatedCrudLayout>
 </template>
