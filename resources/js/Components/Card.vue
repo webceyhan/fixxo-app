78136de7ff1dfe183d63a7a8871983763797219f
Evren Ceyhan <evren@ceyhan.io>
2023-02-27 00:06:02 +0100

    card components impl.

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    label: String,
+});
+</script>
+
+<template>
+    <div
+        class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6"
+    >
+        <slot name="header">
+            <h2
+                v-if="label"
+                class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-6"
+            >
+                {{ label }}
+            </h2>
+        </slot>
+
+        <div class="text-gray-900 dark:text-gray-100">
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ resources/js/Components/StatCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Card from "./Card.vue";
+
+defineProps({
+    label: String,
+});
+</script>
+
+<template>
+    <Card class="border-slate-500 border-l-4">
+        <slot />
+    </Card>
+</template>
