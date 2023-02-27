4e473aac4dbfc432c04cac5b30b273297a46884c
Evren Ceyhan <evren@ceyhan.io>
2023-02-27 10:09:29 +0100

    stat card items via props impl.

--- resources/js/Components/StatCard.vue
+++ resources/js/Components/StatCard.vue
@@ -3,11 +3,38 @@ import Card from "./Card.vue";
 
 defineProps({
     label: String,
+    items: { type: Array, default: () => [] },
 });
 </script>
 
 <template>
     <Card class="border-slate-500 border-l-4">
-        <slot />
+        <h2
+            v-if="label"
+            class="font-semibold uppercase text-lg text-gray-400 leading-tight mb-6"
+        >
+            {{ label }}
+        </h2>
+
+        <slot>
+            <dl
+                class="grid grid-cols-1 sm:grid-cols-2 gap-y-2 sm:divide-x divide-slate-500"
+            >
+                <div
+                    v-for="(item, i) in items"
+                    :key="i"
+                    class="flex justify-between items-center sm:items-start sm:flex-col-reverse first:p-0 sm:pl-8"
+                >
+                    <dt class="text-base leading-7 dark:text-gray-400">
+                        {{ item.label }}
+                    </dt>
+                    <dd
+                        class="text-2xl font-bold leading-9 tracking-tight dark:text-white"
+                    >
+                        {{ item.value }}
+                    </dd>
+                </div>
+            </dl>
+        </slot>
     </Card>
 </template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -19,47 +19,29 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-8">
-                    <StatCard label="Assets">
-                        <dl class="grid grid-cols-1 sm:grid-cols-2 gap-y-2 sm:divide-x divide-slate-500">
-                            <div class="flex justify-between items-center sm:items-start sm:flex-col-reverse first:p-0 sm:pl-8">
-                                <dt class="text-base leading-7 dark:text-gray-400">In Progress</dt>
-                                <dd class="text-2xl font-bold leading-9 tracking-tight dark:text-white">8</dd>
-                            </div>
+                    <StatCard
+                        label="Assets"
+                        :items="[
+                            { label: 'In Progress', value: 8 },
+                            { label: 'Ready', value: 2 },
+                        ]"
+                    />
 
-                            <div class="flex justify-between items-center sm:items-start sm:flex-col-reverse first:p-0 sm:pl-8">
-                                <dt class="text-base leading-7 dark:text-gray-400">Ready</dt>
-                                <dd class="text-2xl font-bold leading-9 tracking-tight dark:text-white">2</dd>
-                            </div>
-                        </dl>
-                    </StatCard>
+                    <StatCard
+                        label="Tasks"
+                        :items="[
+                            { label: 'Pending', value: 10 },
+                            { label: 'Completed', value: 23 },
+                        ]"
+                    />
 
-                    <StatCard label="Tasks">
-                        <dl class="grid grid-cols-1 sm:grid-cols-2 gap-y-2 sm:divide-x divide-slate-500">
-                            <div class="flex justify-between items-center sm:items-start sm:flex-col-reverse first:p-0 sm:pl-8">
-                                <dt class="text-base leading-7 dark:text-gray-400">Pending</dt>
-                                <dd class="text-2xl font-bold leading-9 tracking-tight dark:text-white">10</dd>
-                            </div>
-
-                            <div class="flex justify-between items-center sm:items-start sm:flex-col-reverse first:p-0 sm:pl-8">
-                                <dt class="text-base leading-7 dark:text-gray-400">Completed</dt>
-                                <dd class="text-2xl font-bold leading-9 tracking-tight dark:text-white">23</dd>
-                            </div>
-                        </dl>
-                    </StatCard>
-
-                    <StatCard label="Earnings">
-                        <dl class="grid grid-cols-1 sm:grid-cols-2 gap-y-2 sm:divide-x divide-slate-500">
-                            <div class="flex justify-between items-center sm:items-start sm:flex-col-reverse first:p-0 sm:pl-8">
-                                <dt class="text-base leading-7 dark:text-gray-400">Labor cost</dt>
-                                <dd class="text-2xl font-bold leading-9 tracking-tight dark:text-white">120€</dd>
-                            </div>
-
-                            <div class="flex justify-between items-center sm:items-start sm:flex-col-reverse first:p-0 sm:pl-8">
-                                <dt class="text-base leading-7 dark:text-gray-400">Due paid</dt>
-                                <dd class="text-2xl font-bold leading-9 tracking-tight dark:text-white">300€</dd>
-                            </div>
-                        </dl>
-                     </StatCard>
+                    <StatCard
+                        label="Earnings"
+                        :items="[
+                            { label: 'Labor Cost', value: '120€' },
+                            { label: 'Due Pay', value: '300€' },
+                        ]"
+                    />
                 </div>
             </div>
         </div>
