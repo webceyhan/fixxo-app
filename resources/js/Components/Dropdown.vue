21ff0cfbddb8a02dfba0d9377f5effd8aa9a135c
Evren Ceyhan <evren@ceyhan.io>
2023-03-04 21:43:03 +0100

    dropdown comps cleanup

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,54 +1,75 @@
 <script setup>
-import { computed, onMounted, onUnmounted, ref } from 'vue';
+import { computed, onMounted, onUnmounted, ref } from "vue";
 
 const props = defineProps({
-    align: {
-        default: 'right',
-    },
-    width: {
-        default: '48',
-    },
+    label: String,
+    align: { default: "right" },
+    width: { default: "48" },
     contentClasses: {
-        default: () => ['py-1', 'bg-white dark:bg-gray-700'],
+        default: () => ["py-1", "bg-white dark:bg-gray-700"],
     },
 });
 
+const open = ref(false);
+
 const closeOnEscape = (e) => {
-    if (open.value && e.key === 'Escape') {
+    if (open.value && e.key === "Escape") {
         open.value = false;
     }
 };
 
-onMounted(() => document.addEventListener('keydown', closeOnEscape));
-onUnmounted(() => document.removeEventListener('keydown', closeOnEscape));
+onMounted(() => document.addEventListener("keydown", closeOnEscape));
+onUnmounted(() => document.removeEventListener("keydown", closeOnEscape));
 
 const widthClass = computed(() => {
-    return {
-        48: 'w-48',
-    }[props.width.toString()];
+    return { 48: "w-48" }[props.width.toString()];
 });
 
 const alignmentClasses = computed(() => {
-    if (props.align === 'left') {
-        return 'origin-top-left left-0';
-    } else if (props.align === 'right') {
-        return 'origin-top-right right-0';
+    if (props.align === "left") {
+        return "origin-top-left left-0";
+    } else if (props.align === "right") {
+        return "origin-top-right right-0";
     } else {
-        return 'origin-top';
+        return "origin-top";
     }
 });
-
-const open = ref(false);
 </script>
 
 <template>
     <div class="relative">
         <div @click="open = !open">
-            <slot name="trigger" />
+            <slot name="trigger">
+                <span class="inline-flex rounded-md">
+                    <button
+                        type="button"
+                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                    >
+                        {{ label }}
+
+                        <svg
+                            class="ml-2 -mr-0.5 h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                    </button>
+                </span>
+            </slot>
         </div>
 
         <!-- Full Screen Dropdown Overlay -->
-        <div v-show="open" class="fixed inset-0 z-40" @click="open = false"></div>
+        <div
+            v-show="open"
+            class="fixed inset-0 z-40"
+            @click="open = false"
+        ></div>
 
         <transition
             enter-active-class="transition ease-out duration-200"
@@ -65,8 +86,11 @@ const open = ref(false);
                 style="display: none"
                 @click="open = false"
             >
-                <div class="rounded-md ring-1 ring-black ring-opacity-5" :class="contentClasses">
-                    <slot name="content" />
+                <div
+                    class="rounded-md ring-1 ring-black dark:ring-gray-500 ring-opacity-5"
+                    :class="contentClasses"
+                >
+                    <slot />
                 </div>
             </div>
         </transition>
--- resources/js/Layouts/Partials/NavBar.vue
+++ resources/js/Layouts/Partials/NavBar.vue
@@ -96,43 +96,17 @@ const allowedLinks = computed(() =>
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     <!-- Settings Dropdown -->
                     <div class="ml-3 relative">
-                        <Dropdown align="right" width="48">
-                            <template #trigger>
-                                <span class="inline-flex rounded-md">
-                                    <button
-                                        type="button"
-                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
-                                    >
-                                        {{ $page.props.auth.user.name }}
+                        <Dropdown :label="$page.props.auth.user.name">
+                            <DropdownLink
+                                label="Profile"
+                                :href="route('profile.edit')"
+                            />
 
-                                        <svg
-                                            class="ml-2 -mr-0.5 h-4 w-4"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20"
-                                            fill="currentColor"
-                                        >
-                                            <path
-                                                fill-rule="evenodd"
-                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"
-                                            />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </template>
-
-                            <template #content>
-                                <DropdownLink :href="route('profile.edit')">
-                                    Profile
-                                </DropdownLink>
-                                <DropdownLink
-                                    :href="route('logout')"
-                                    method="post"
-                                    as="button"
-                                >
-                                    Log Out
-                                </DropdownLink>
-                            </template>
+                            <DropdownLink
+                                label="Log Out"
+                                method="post"
+                                :href="route('logout')"
+                            />
                         </Dropdown>
                     </div>
                 </div>
@@ -211,16 +185,16 @@ const allowedLinks = computed(() =>
                 </div>
 
                 <div class="mt-3 space-y-1">
-                    <ResponsiveNavLink :href="route('profile.edit')">
-                        Profile
-                    </ResponsiveNavLink>
                     <ResponsiveNavLink
-                        :href="route('logout')"
+                        label="Profile"
+                        :href="route('profile.edit')"
+                    />
+
+                    <ResponsiveNavLink
+                        label="Log Out"
                         method="post"
-                        as="button"
-                    >
-                        Log Out
-                    </ResponsiveNavLink>
+                        :href="route('logout')"
+                    />
                 </div>
             </div>
         </div>
