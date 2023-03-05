e7b665f2845e52c67b47e14907c5bf280a99522c
Evren Ceyhan <evren@ceyhan.io>
2023-03-05 20:05:08 +0100

    description list href prop added

--- resources/js/Components/List/DescriptionListItem.vue
+++ resources/js/Components/List/DescriptionListItem.vue
@@ -10,63 +10,65 @@ import Link from "@/Components/Link.vue";
 defineProps({
     label: String,
     type: String,
+    href: String,
     value: [String, Number],
 });
 </script>
 
 <template>
     <div class="flex flex-col lg:flex-row overflow-hidden gap-2 py-4">
-        <slot>
-            <dt class="sm:w-2/5 text-gray-500 dark:text-gray-400 capitalize">
-                <slot name="label">{{ label }}</slot>
-            </dt>
+        <dt class="sm:w-2/5 text-gray-500 dark:text-gray-400 capitalize">
+            <slot name="label">{{ label }}</slot>
+        </dt>
 
-            <dd class="sm:w-3/5 text-gray-900 dark:text-gray-100 truncate">
-                <slot name="value">
-                    <!-- format as date / datetime -->
-                    <span v-if="type === 'date'">
-                        {{ formatDate(value) }}
-                    </span>
+        <dd class="sm:w-3/5 text-gray-900 dark:text-gray-100 truncate">
+            <slot>
+                <!-- format as date / datetime -->
+                <span v-if="type === 'date'">
+                    {{ formatDate(value) }}
+                </span>
 
-                    <!-- format as money -->
-                    <span v-else-if="type === 'money'">
-                        {{ formatMoney(value) }}
-                    </span>
+                <!-- format as money -->
+                <span v-else-if="type === 'money'">
+                    {{ formatMoney(value) }}
+                </span>
 
-                    <!-- format as number -->
-                    <span v-else-if="type === 'number'">
-                        {{ formatNumber(value) }}
-                    </span>
+                <!-- format as number -->
+                <span v-else-if="type === 'number'">
+                    {{ formatNumber(value) }}
+                </span>
 
-                    <!-- format as phone number -->
-                    <Link
-                        v-else-if="type === 'phone'"
-                        :label="formatPhone(value)"
-                        :href="`tel:${value}`"
-                        outline
-                    />
+                <!-- format as phone number -->
+                <Link
+                    v-else-if="type === 'phone'"
+                    :label="formatPhone(value)"
+                    :href="`tel:${value}`"
+                    outline
+                />
 
-                    <!-- render as email -->
-                    <Link
-                        v-else-if="type === 'email'"
-                        :label="value"
-                        :href="`mailto:${value}`"
-                        outline
-                    />
+                <!-- render as email -->
+                <Link
+                    v-else-if="type === 'email'"
+                    :label="value"
+                    :href="`mailto:${value}`"
+                    outline
+                />
 
-                    <!-- render as location -->
-                    <Link
-                        v-else-if="type === 'location'"
-                        :label="value"
-                        :href="`https://www.google.com/maps/place/${value}`"
-                        target="_blank"
-                        outline
-                    />
+                <!-- render as location -->
+                <Link
+                    v-else-if="type === 'location'"
+                    :label="value"
+                    :href="`https://www.google.com/maps/place/${value}`"
+                    target="_blank"
+                    outline
+                />
 
-                    <!-- render as-is -->
-                    <span v-else> {{ value }} </span>
-                </slot>
-            </dd>
-        </slot>
+                <!-- render as link -->
+                <Link v-else-if="href" :label="value" :href="href" outline />
+
+                <!-- render as-is -->
+                <span v-else> {{ value }} </span>
+            </slot>
+        </dd>
     </div>
 </template>
