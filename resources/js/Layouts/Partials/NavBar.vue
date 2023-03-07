ebfc38c0e57402666acb8d7e4bf9b5ff81efd9d9
Evren Ceyhan <evren@ceyhan.io>
2023-03-07 10:10:32 +0100

    tasks, payments links removed from navbar

--- resources/js/Layouts/Partials/NavBar.vue
+++ resources/js/Layouts/Partials/NavBar.vue
@@ -9,196 +9,165 @@ import NavLink from "./NavLink.vue";
 import ResponsiveNavLink from "./ResponsiveNavLink.vue";
 
 const props = defineProps({
-    user: Object,
+  user: Object,
 });
 
 const toggled = ref(false);
 
 const links = [
-    {
-        label: "New",
-        // icon: "create",
-        to: "customers.create",
-    },
-    {
-        label: "Dashboard",
-        // icon: "dashboard",
-        to: "dashboard",
-        admin: true,
-    },
-    {
-        label: "Customers",
-        // icon: "customer",
-        to: "customers.index",
-    },
-    {
-        label: "Assets",
-        // icon: "asset",
-        to: "assets.index",
-    },
-    {
-        label: "Tasks",
-        // icon: "task",
-        to: "tasks.index",
-        // class: "hidden md:inline-flex",
-    },
-    {
-        label: "Payments",
-        // icon: "payment",
-        to: "payments.index",
-        // class: "hidden md:inline-flex",
-    },
-    {
-        label: "Users",
-        // icon: "user",
-        to: "users.index",
-        // class: "hidden md:inline-flex",
-        admin: true,
-    },
+  {
+    label: "New",
+    // icon: "create",
+    to: "customers.create",
+  },
+  {
+    label: "Dashboard",
+    // icon: "dashboard",
+    to: "dashboard",
+    admin: true,
+  },
+  {
+    label: "Customers",
+    // icon: "customer",
+    to: "customers.index",
+  },
+  {
+    label: "Assets",
+    // icon: "asset",
+    to: "assets.index",
+  },
+  {
+    label: "Users",
+    // icon: "user",
+    to: "users.index",
+    // class: "hidden md:inline-flex",
+    admin: true,
+  },
 ];
 
 const allowedLinks = computed(() =>
-    links.filter(({ admin }) => !(admin && !isAdmin.value))
+  links.filter(({ admin }) => !(admin && !isAdmin.value))
 );
 </script>
 
 <template>
-    <nav
-        class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700"
+  <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <!-- Primary Navigation Menu -->
+    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between h-16">
+        <div class="flex">
+          <!-- Logo -->
+          <div class="shrink-0 flex items-center">
+            <Link :href="route('dashboard')">
+              <Logo
+                class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
+              />
+            </Link>
+          </div>
+
+          <!-- Navigation Links -->
+          <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <NavLink
+              v-for="(link, index) in allowedLinks"
+              :key="index"
+              :href="route(link.to)"
+              :active="route().current(link.to)"
+              :class="link.class"
+              :label="link.label"
+            />
+          </div>
+        </div>
+
+        <div class="hidden sm:flex sm:items-center sm:ml-6">
+          <!-- Settings Dropdown -->
+          <div class="ml-3 relative">
+            <Dropdown :label="$page.props.auth.user.name">
+              <DropdownLink label="Profile" :href="route('profile.edit')" />
+
+              <DropdownLink
+                label="Log Out"
+                method="post"
+                as="button"
+                :href="route('logout')"
+              />
+            </Dropdown>
+          </div>
+        </div>
+
+        <!-- Hamburger -->
+        <div class="-mr-2 flex items-center sm:hidden">
+          <button
+            @click="toggled = !toggled"
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
+          >
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <path
+                :class="{
+                  hidden: toggled,
+                  'inline-flex': !toggled,
+                }"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+              <path
+                :class="{
+                  hidden: !toggled,
+                  'inline-flex': toggled,
+                }"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Responsive Navigation Menu -->
+    <div
+      :class="{
+        block: toggled,
+        hidden: !toggled,
+      }"
+      class="sm:hidden"
     >
-        <!-- Primary Navigation Menu -->
-        <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="flex justify-between h-16">
-                <div class="flex">
-                    <!-- Logo -->
-                    <div class="shrink-0 flex items-center">
-                        <Link :href="route('dashboard')">
-                            <Logo
-                                class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"
-                            />
-                        </Link>
-                    </div>
+      <div class="pt-2 pb-3 space-y-1">
+        <ResponsiveNavLink
+          v-for="(link, index) in allowedLinks"
+          :key="index"
+          :href="route(link.to)"
+          :active="route().current(link.to)"
+          :class="link.class"
+          :label="link.label"
+        />
+      </div>
 
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <NavLink
-                            v-for="(link, index) in allowedLinks"
-                            :key="index"
-                            :href="route(link.to)"
-                            :active="route().current(link.to)"
-                            :class="link.class"
-                            :label="link.label"
-                        />
-                            
-                        
-                    </div>
-                </div>
-
-                <div class="hidden sm:flex sm:items-center sm:ml-6">
-                    <!-- Settings Dropdown -->
-                    <div class="ml-3 relative">
-                        <Dropdown :label="$page.props.auth.user.name">
-                            <DropdownLink
-                                label="Profile"
-                                :href="route('profile.edit')"
-                            />
-
-                            <DropdownLink
-                                label="Log Out"
-                                method="post"
-                                as="button"
-                                :href="route('logout')"
-                            />
-                        </Dropdown>
-                    </div>
-                </div>
-
-                <!-- Hamburger -->
-                <div class="-mr-2 flex items-center sm:hidden">
-                    <button
-                        @click="toggled = !toggled"
-                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
-                    >
-                        <svg
-                            class="h-6 w-6"
-                            stroke="currentColor"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                :class="{
-                                    hidden: toggled,
-                                    'inline-flex': !toggled,
-                                }"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16"
-                            />
-                            <path
-                                :class="{
-                                    hidden: !toggled,
-                                    'inline-flex': toggled,
-                                }"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12"
-                            />
-                        </svg>
-                    </button>
-                </div>
-            </div>
+      <!-- Responsive Settings Options -->
+      <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+        <div class="px-4">
+          <div class="font-medium text-base text-gray-800 dark:text-gray-200">
+            {{ $page.props.auth.user.name }}
+          </div>
+          <div class="font-medium text-sm text-gray-500">
+            {{ $page.props.auth.user.email }}
+          </div>
         </div>
 
-        <!-- Responsive Navigation Menu -->
-        <div
-            :class="{
-                block: toggled,
-                hidden: !toggled,
-            }"
-            class="sm:hidden"
-        >
-            <div class="pt-2 pb-3 space-y-1">
-                <ResponsiveNavLink
-                    v-for="(link, index) in allowedLinks"
-                    :key="index"
-                    :href="route(link.to)"
-                    :active="route().current(link.to)"
-                    :class="link.class"
-                    :label="link.label"
-                />
-            </div>
+        <div class="mt-3 space-y-1">
+          <ResponsiveNavLink label="Profile" :href="route('profile.edit')" />
 
-            <!-- Responsive Settings Options -->
-            <div
-                class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600"
-            >
-                <div class="px-4">
-                    <div
-                        class="font-medium text-base text-gray-800 dark:text-gray-200"
-                    >
-                        {{ $page.props.auth.user.name }}
-                    </div>
-                    <div class="font-medium text-sm text-gray-500">
-                        {{ $page.props.auth.user.email }}
-                    </div>
-                </div>
-
-                <div class="mt-3 space-y-1">
-                    <ResponsiveNavLink
-                        label="Profile"
-                        :href="route('profile.edit')"
-                    />
-
-                    <ResponsiveNavLink
-                        label="Log Out"
-                        method="post"
-                        as="button"
-                        :href="route('logout')"
-                    />
-                </div>
-            </div>
+          <ResponsiveNavLink
+            label="Log Out"
+            method="post"
+            as="button"
+            :href="route('logout')"
+          />
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </template>
