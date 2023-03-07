16a393cc9dd6fd83869fb8ce0b23619391ecf543
Evren Ceyhan <evren@ceyhan.io>
2023-03-07 19:44:42 +0100

    bugfix added for mysql-db on digitalocean

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Database\Events\MigrationsStarted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,7 +16,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // TODO: bugfix for DigitalOcean Mysql Server issue
+        Event::listen(MigrationsStarted::class, function (){
+            if (env('ALLOW_DISABLED_PK')) {
+                DB::statement('SET SESSION sql_require_primary_key=0');
+            }
+        });
+        
+        // TODO: bugfix for DigitalOcean Mysql Server issue
+        Event::listen(MigrationsEnded::class, function (){
+            if (env('ALLOW_DISABLED_PK')) {
+                DB::statement('SET SESSION sql_require_primary_key=1');
+            }
+        });
     }
 
     /**
@@ -20,6 +36,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // TODO: fix for Heroku server
         if(config('app.env') === 'production') {
             URL::forceScheme('https');
          }
