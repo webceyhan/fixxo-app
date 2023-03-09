053f179ab38cdde2c0fe5dbc056410ab854216fc
Evren Ceyhan <evren@ceyhan.io>
2023-03-09 09:46:56 +0100

    dropdown left alignment added

--- resources/js/Components/Menu/Dropdown.vue
+++ resources/js/Components/Menu/Dropdown.vue
@@ -7,28 +7,29 @@ defineProps({
   label: String,
   icon: String,
   theme: { type: String, default: "secondary" },
+  left: Boolean,
 });
 </script>
 
 <template>
   <Menu as="div" class="relative inline-block text-left">
     <div>
-      <slot name="trigger">
-        <!-- normal button with label -->
-        <MenuButton v-if="label" :as="SecondaryButton">
-          {{ label }}
+      <!-- normal button with label -->
+      <MenuButton v-if="label" :as="SecondaryButton">
+        {{ label }}
 
-          <Icon name="chevron-down" class="-mr-1 opacity-50" aria-hidden="true" />
-        </MenuButton>
+        <Icon name="chevron-down" class="-mr-1 opacity-50" aria-hidden="true" />
+      </MenuButton>
 
-        <!-- menu icon button -->
-        <MenuButton v-else>
+      <!-- menu icon button -->
+      <MenuButton v-else>
+        <slot name="trigger">
           <Icon
-            name="three-dots-vertical"
+            :name="icon ?? 'three-dots-vertical'"
             class="text-white p-2 hover:bg-gray-800 rounded"
           />
-        </MenuButton>
-      </slot>
+        </slot>
+      </MenuButton>
     </div>
 
     <transition
@@ -40,7 +41,11 @@ defineProps({
       leave-to-class="transform scale-95 opacity-0"
     >
       <MenuItems
-        class="absolute right-0 mt-2 w-56 origin-top-right divide-y dark:divide-gray-900 rounded-md bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black dark:ring-gray-500 ring-opacity-5 focus:outline-none"
+        class="z-50 absolute mt-2 w-56 divide-y dark:divide-gray-900 rounded-md bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black dark:ring-gray-500 ring-opacity-5 focus:outline-none"
+        :class="{
+          'right-0 origin-top-right': !left,
+          'left-0 origin-top-left': left,
+        }"
       >
         <slot />
       </MenuItems>
