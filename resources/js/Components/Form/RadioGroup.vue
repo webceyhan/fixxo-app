c188aabeb20ee242926dba9b200c90d57c1f8606
Evren Ceyhan <evren@ceyhan.io>
2023-03-11 18:22:13 +0100

    radiogroup options normalized

--- resources/js/Components/Form/RadioGroup.vue
+++ resources/js/Components/Form/RadioGroup.vue
@@ -3,6 +3,7 @@ let index = 0;
 </script>
 
 <script setup>
+import { normalizeOptions } from "@/Shared/form";
 import { RadioGroup, RadioGroupOption } from "@headlessui/vue";
 
 const id = `radio-group-${index++}`;
@@ -24,9 +25,9 @@ defineEmits(["change", "update:modelValue"]);
       class="flex text-sm font-semibold text-gray-600 dark:text-gray-300 shadow-sm"
     >
       <RadioGroupOption
-        v-for="(option, key) in options"
-        :key="key"
-        :value="key"
+        v-for="(option, i) in normalizeOptions(options)"
+        :key="i"
+        :value="option.value"
         v-slot="{ checked }"
         as="template"
       >
@@ -37,7 +38,7 @@ defineEmits(["change", "update:modelValue"]);
             'text-white border-transparent bg-indigo-500 dark:bg-indigo-600': checked,
           }"
         >
-          {{ option ?? key }}
+          {{ option.label }}
         </div>
       </RadioGroupOption>
     </RadioGroup>
@@ -45,20 +46,20 @@ defineEmits(["change", "update:modelValue"]);
 
   <div v-else class="flex flex-col md:flex-row gap-1 md:gap-5">
     <label
-      v-for="(option, key) in options"
-      :key="key"
+      v-for="(option, i) in normalizeOptions(options)"
+      :key="i"
       class="inline-flex items-center mt-3 sm:m-0"
     >
       <input
         type="radio"
         :name="id"
-        :value="key"
-        :checked="key === modelValue"
-        @change="$emit('update:modelValue', $event.target.value)"
+        :value="option.value"
+        :checked="option.active"
+        @change="$emit('update:modelValue', option.value)"
         class="form-radio h-5 w-5 text-gray-600"
       />
       <span class="ml-2 text-gray-500">
-        {{ option ?? key }}
+        {{ option.label }}
       </span>
     </label>
   </div>
