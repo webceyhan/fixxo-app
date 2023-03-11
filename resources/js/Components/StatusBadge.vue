581bb6dadc803b15d99f2d104ac0248b85e413dd
Evren Ceyhan <evren@ceyhan.io>
2023-03-11 23:56:30 +0100

    compact mode added to status badge

--- resources/js/Components/StatusBadge.vue
+++ resources/js/Components/StatusBadge.vue
@@ -6,6 +6,7 @@ defineProps({
   label: String,
   icon: String,
   theme: String,
+  compact: Boolean,
 });
 
 // common icons by theme
@@ -19,10 +20,10 @@ const iconMap = {
 </script>
 
 <template>
-  <Badge :theme="theme" class="whitespace-nowrap">
+  <Badge :theme="theme" class="whitespace-nowrap" :class="{ '!p-0.5': compact }">
     <!-- icon only visible as badge on mobile -->
-    <Icon :name="icon ?? iconMap[theme]" class="lg:hidden" />
+    <Icon :name="icon ?? iconMap[theme]" :class="{ 'lg:hidden': !compact }" />
     <!-- label only visible on larger screens -->
-    <span class="hidden lg:inline">{{ label }}</span>
+    <span v-if="!compact" class="hidden lg:inline">{{ label }}</span>
   </Badge>
 </template>
