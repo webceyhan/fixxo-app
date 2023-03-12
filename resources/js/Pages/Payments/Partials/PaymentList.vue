f43d979cc86228aec99105a08e93e3ba52dd2d9f
Evren Ceyhan <evren@ceyhan.io>
2023-03-12 22:58:38 +0100

    payment list item default title added

--- resources/js/Pages/Payments/Partials/PaymentList.vue
+++ resources/js/Pages/Payments/Partials/PaymentList.vue
@@ -22,7 +22,7 @@ defineProps({
     >
       <div class="w-full truncate">
         <span>
-          {{ payment.notes }}
+          {{ payment.notes ?? payment.type }}
         </span>
 
         <div class="hidden md:block text-gray-400 text-sm mt-1">
