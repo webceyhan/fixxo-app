cc3fa2a620681dd286c42f769be89feeb93424a7
Evren Ceyhan <evren@ceyhan.io>
2023-03-19 18:27:22 +0100

    progress bar comp. added

---
+++ resources/js/Components/ProgressBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  value: Number,
+});
+</script>
+
+<template>
+  <div class="h-1 bg-gray-200/50 dark:bg-gray-800/50">
+    <span
+      class="h-full bg-blue-500/50 dark:bg-blue-500/50"
+      :style="{ width: `${value}%` }"
+    />
+  </div>
+</template>
