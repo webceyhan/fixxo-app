8c489c9b8ea3881169986177f2086c8b19fcc5de
Evren Ceyhan <evren@ceyhan.io>
2023-03-20 16:26:09 +0100

    layout sections refactored

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -18,28 +18,31 @@ const currentTitle = computed(
 </script>
 
 <template>
-  <div class="flex h-screen bg-gray-200 dark:bg-gray-900">
+  <div class="flex h-screen bg-gray-200 dark:bg-gray-900 overflow-x-hidden">
     <Head :title="currentTitle" />
 
-    <!-- Side Navigation Menu -->
+    <!-- Side Navigation -->
     <Sidebar v-model:toggled="sidebarOpen" />
 
-    <div class="flex-1 flex flex-col overflow-x-hidden overflow-y-auto">
-      <!-- Primary Navigation -->
+    <div class="flex-1 overflow-x-hidden overflow-y-auto">
+      <!-- Top Navigation -->
       <NavBar :user="$page.props.auth.user" @toggle="sidebarOpen = true" />
 
-      <!-- Page Heading -->
-      <header class="container mx-auto p-4 sm:p-6 pb-0 sm:pb-0">
-        <Breadcrumbs :links="$page.props.breadcrumbs" class="hidden md:flex" />
-        <h2 class="text-2xl dark:text-white md:hidden">{{ currentTitle }}</h2>
-      </header>
+      <!-- Page Wrapper -->
+      <div class="container mx-auto p-4 sm:p-6 space-y-6">
+        <!-- Page Heading -->
+        <header>
+          <Breadcrumbs :links="$page.props.breadcrumbs" class="hidden md:flex" />
+          <h2 class="text-2xl dark:text-white md:hidden">
+            <slot name="title">{{ currentTitle }}</slot>
+          </h2>
+        </header>
 
-      <!-- Page Content -->
-      <main class="container mx-auto p-4 sm:p-6 space-y-6">
-        <slot />
-      </main>
+        <!-- Page Content -->
+        <main class="space-y-6"><slot /></main>
+      </div>
 
-      <!-- notifications -->
+      <!-- Notifications -->
       <NotificationBar :message="$page.props.flash.status" />
     </div>
   </div>
