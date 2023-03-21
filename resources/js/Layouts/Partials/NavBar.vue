05caae73d54e406318d9c26c4bc59f6b004a97bc
Evren Ceyhan <evren@ceyhan.io>
2023-03-21 19:27:35 +0100

    navbar height adjusted

--- resources/js/Layouts/Partials/NavBar.vue
+++ resources/js/Layouts/Partials/NavBar.vue
@@ -15,25 +15,20 @@ const props = defineProps({
   <nav
     class="bg-white dark:bg-gray-800/50 border-b-4 border-indigo-500 dark:border-indigo-900 shadow-md z-10"
   >
-    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex items-center justify-between gap-4 h-16">
-        <!-- Hamburger -->
-        <button
-          @click="$emit('toggle')"
-          class="text-gray-500 focus:outline-none lg:hidden"
-        >
-          <Icon class="text-3xl" name="list" />
-        </button>
+    <div class="container mx-auto flex items-center gap-4 py-3 px-4 sm:px-6 lg:px-8">
+      <!-- Hamburger -->
+      <button @click="$emit('toggle')" class="text-gray-500 focus:outline-none lg:hidden">
+        <Icon class="text-3xl" name="list" />
+      </button>
 
-        <!-- SearchBar -->
-        <Searchbar class="mr-auto" />
+      <!-- SearchBar -->
+      <Searchbar class="mr-auto" />
 
-        <!-- Notification Menu -->
-        <NotificationMenu class="hidden sm:inline sm:mx-4 lg:mx-6" />
+      <!-- Notification Menu -->
+      <NotificationMenu class="hidden sm:inline sm:mx-4 lg:mx-6" />
 
-        <!-- User Menu -->
-        <UserMenu :user="user" class="flex-shrink-0" />
-      </div>
+      <!-- User Menu -->
+      <UserMenu :user="user" class="flex-shrink-0" />
     </div>
   </nav>
 </template>
