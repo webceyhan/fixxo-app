40249e68829371b57c67fe490501e6810b238cb7
Evren Ceyhan <evren@ceyhan.io>
2023-03-24 17:35:06 +0100

    income chart component impl.

---
+++ resources/js/Pages/Dashboard/Partials/IncomeChart.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref, computed, onMounted, watch } from "vue";
+import {
+  Chart,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  LineController,
+} from "chart.js";
+
+Chart.register(LineElement, PointElement, LinearScale, CategoryScale, LineController);
+
+const props = defineProps({
+  labels: Array,
+  series: Array,
+});
+
+const canvasRef = ref(null);
+const chartInstance = ref(null);
+
+const data = computed(() => ({
+  labels: props.labels,
+  datasets: [
+    {
+      data: props.series,
+      backgroundColor: "#dc3545",
+      borderColor: "#dc3545",
+      tension: 0.5,
+    },
+  ],
+}));
+
+const drawChart = () => {
+  if (chartInstance.value) {
+    chartInstance.value.destroy();
+  }
+
+  chartInstance.value = new Chart(canvasRef.value, {
+    type: "line",
+    data: data.value,
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        y: {
+          ticks: {
+            maxTicksLimit: 5,
+            callback: (value, index, ticks) => `€${value}`,
+          },
+        },
+      },
+    },
+  });
+};
+
+onMounted(() => drawChart());
+
+// re-render chart when data changes
+watch(data, () => drawChart());
+</script>
+
+<template>
+  <canvas ref="canvasRef" class="w-full max-h-48"  />
+</template>
