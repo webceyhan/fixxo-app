5cda517afd3a2038188cdf9bc4cdd48dd74eddbd
Evren Ceyhan <evren@ceyhan.io>
2023-03-24 23:39:07 +0100

    task status enum todo added

--- app/Enums/TaskStatus.php
+++ app/Enums/TaskStatus.php
@@ -4,6 +4,8 @@ namespace App\Enums;
 
 class TaskStatus extends Enum
 {
+    // TODO: add more declarative statuses
+    // or replace it with a completed_at timestamp
     const PENDING = 'pending';
     const DONE = 'done';
 }
