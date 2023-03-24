c17a117e6077263dd99e5bef3625c90c2dbb02f5
Evren Ceyhan <evren@ceyhan.io>
2023-03-24 15:45:38 +0100

    tabnav light mode border adjusted

--- resources/js/Components/Nav/TabNav.vue
+++ resources/js/Components/Nav/TabNav.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 <template>
   <nav
     role="tablist"
-    class="text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700"
+    class="text-center text-gray-500 border-b border-slate-100 dark:text-gray-400 dark:border-slate-700"
   >
     <ul class="flex flex-wrap -mb-px">
       <slot>
