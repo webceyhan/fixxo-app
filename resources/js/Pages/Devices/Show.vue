87a9bf8bba55be8c05d1ecbe5d024b0af91ca938
Evren Ceyhan <evren@ceyhan.io>
2023-03-25 23:06:59 +0100

    history list ui refactored

--- resources/js/Pages/Devices/Show.vue
+++ resources/js/Pages/Devices/Show.vue
@@ -134,27 +134,28 @@ const save = () => {
         :can-delete="canDeleteTicket"
       /> -->
 
-      <Card label="History" class="!bg-opacity-50">
-        <ul class="divide-y divide-gray-700/50">
-          <li
-            v-for="log in device.logs"
-            :key="log.id"
-            class="grid grid-cols-3 text-sm text-gray-500 p-4"
-          >
-            <div>
-              <DeviceBadge :status="log.status" />
-            </div>
-
-            <div>
-              {{ log.user.name }}
-            </div>
-
-            <div>
+      <!-- <Card label="History" class="!bg-opacity-25">  -->
+      <h3 class="text-xl font-semibold dark:text-white pt-4">History</h3>
+        <ol class="relative border-l border-gray-50 dark:border-gray-700 space-y-10">
+          <li v-for="log in device.logs" :key="log.id" class="ml-4">
+            <div
+              class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
+            ></div>
+            <time
+              class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+            >
               {{ formatDate(log.created_at, true) }}
-            </div>
+            </time>
+            <p
+              class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 space-x-2"
+            >
+              <span class="text-black dark:text-gray-200">{{ log.user.name }}</span>
+              <span>changed status to</span>
+              <DeviceBadge :status="log.status" class="dark:!bg-black/25" />
+            </p>
           </li>
-        </ul>
-      </Card>
+        </ol>
+      <!-- </Card> -->
     </template>
   </PageLayout>
 </template>
