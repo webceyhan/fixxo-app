4aed9c1e1cbbf5306ab3b7c524ad7de027bdb416
Evren Ceyhan <evren@ceyhan.io>
2023-03-25 18:34:51 +0100

    controller added

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\DeviceStatus;
+use App\Enums\DeviceType;
+use App\Http\Requests\SaveDeviceRequest;
+use App\Models\Device;
+
+class DeviceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $allowedParams = request()->only('search', 'type', 'status', 'brand');
+
+        $devices = Device::query()
+            ->filterByParams($allowedParams)
+            // ->withCount(['tasks'])
+            // ->withSum('tasks as total_cost', 'price')
+            ->with('customer:id,name')
+            ->latest('id')
+            ->paginate()
+            ->withQueryString();
+
+        // Get all distinct brands to use as filter options
+        $brands = Device::brands()->get();
+
+        return inertia('Devices/Index', [
+            'devices' => $devices,
+            'filters' => [
+                'type' => DeviceType::values(),
+                'status' => DeviceStatus::values(),
+                'brand' => $brands->pluck('brand'),
+            ]
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(SaveDeviceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Device $device)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Device $device)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(SaveDeviceRequest $request, Device $device)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Device $device)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/SaveDeviceRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\DeviceStatus;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SaveDeviceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        // update
+        if ($this->isMethod('put')) {
+            return [
+                'name' => 'nullable|string',
+                'brand' => 'nullable|string',
+                'type' => 'nullable|string',
+                'serial' => 'nullable|string',
+                'purchase_date' => 'nullable|date',
+                'warranty_expire_date' => 'nullable|date',
+                'status' => ['nullable', Rule::in(DeviceStatus::values())],
+            ];
+        }
+
+        // store
+        return [
+            'customer_id' => 'required_without:id',
+            'name' => 'required|string',
+            'brand' => 'nullable|string',
+            'type' => 'nullable|string',
+            'serial' => 'nullable|string',
+            'purchase_date' => 'nullable|date',
+            'warranty_expire_date' => 'nullable|date',
+            'status' => ['nullable', Rule::in(DeviceStatus::values())],
+        ];
+    }
+}
