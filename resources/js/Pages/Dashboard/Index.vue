394468d9628df61de5c6c4cb5e899ecd41dd160b
Evren Ceyhan <evren@ceyhan.io>
2023-03-25 11:52:59 +0100

    income chart card label added

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -56,9 +56,9 @@ const onIntervalChange = (interval) => {
       />
     </div>
 
-    <Card>
+    <StatCard label="Income">
       <IncomeChart v-bind="incomeStats" />
-    </Card>
+    </StatCard>
 
     <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6 lg:gap-8">
       <SingleStatCard
