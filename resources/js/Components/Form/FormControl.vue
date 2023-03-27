f3d9ecc86c624bf1b6ed87090bc191166d74d3a5
Evren Ceyhan <evren@ceyhan.io>
2023-03-27 11:46:01 +0200

    chekbox added to formcontrol

--- resources/js/Components/Form/FormControl.vue
+++ resources/js/Components/Form/FormControl.vue
@@ -2,7 +2,8 @@
 let index = 0;
 
 export default {
-    inheritAttrs: false,
+  inheritAttrs: false,
+  components: { Checkbox },
 };
 </script>
 
@@ -13,37 +14,55 @@ import Textarea from "./Textarea.vue";
 import TextInput from "./TextInput.vue";
 import InputLabel from "./InputLabel.vue";
 import InputError from "./InputError.vue";
+import Checkbox from "./Checkbox.vue";
 
 const id = `form-control-${index++}`;
 
 const input = ref(null);
 
 const props = defineProps({
-    label: String,
-    error: String,
+  label: String,
+  error: String,
 });
 
 const attrs = computed(() => ({
-    id,
-    name: id,
-    ...useAttrs(),
+  id,
+  name: id,
+  ...useAttrs(),
 }));
 
 defineExpose({
-    focus: () => input.value.focus(),
+  focus: () => input.value.focus(),
 });
 </script>
 
 <template>
-    <div>
-        <InputLabel :for="id" :value="label" />
-
-        <Select ref="input" v-if="$attrs.options" v-bind="attrs" class="mt-1 block w-full"/>
-
-        <Textarea ref="input" v-else-if="$attrs.rows" v-bind="attrs" class="mt-1 block w-full"/>
-
-        <TextInput ref="input" v-else v-bind="attrs" class="mt-1 block w-full"/>
-
-        <InputError :message="error" class="mt-2"/>
+  <div>
+    <div v-if="$attrs.type == 'checkbox'" class="flex items-center mt-1 gap-2">
+      <Checkbox ref="input" v-bind="attrs" />
+      <InputLabel :for="id" :value="label" />
     </div>
+
+    <template v-else>
+      <InputLabel :for="id" :value="label" />
+
+      <Select
+        v-if="$attrs.options"
+        v-bind="attrs"
+        ref="input"
+        class="mt-1 block w-full"
+      />
+
+      <Textarea
+        v-else-if="$attrs.rows"
+        v-bind="attrs"
+        ref="input"
+        class="mt-1 block w-full"
+      />
+
+      <TextInput v-else v-bind="attrs" ref="input" class="mt-1 block w-full" />
+
+      <InputError :message="error" class="mt-2" />
+    </template>
+  </div>
 </template>
