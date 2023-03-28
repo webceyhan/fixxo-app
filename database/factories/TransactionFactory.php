3197da4850f610fccb9e7ae98e41655f256dcd4d
Evren Ceyhan <evren@ceyhan.io>
2023-03-28 16:18:43 +0200

    payment factory states added

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -19,10 +19,34 @@ class TransactionFactory extends Factory
     public function definition(): array
     {
         return [
-            'amount' => fake()->randomFloat(2, 0, 100),
+            'amount' => fake()->randomFloat(2, 10, 100),
             'type' => fake()->randomElement(TransactionType::values()),
             'method' => fake()->randomElement(TransactionMethod::values()),
             'note' => fake()->optional(.2)->text,
         ];
+    }
+
+    /**
+     * Indicate that the model's type is discount.
+     *
+     * @return $this
+     */
+    public function discount(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'type' => TransactionType::DISCOUNT,
+        ]);
+    }
+
+    /**
+     * Indicate that the model's type is payment.
+     *
+     * @return $this
+     */
+    public function payment(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'type' => TransactionType::PAYMENT,
+        ]);
     }
 }
