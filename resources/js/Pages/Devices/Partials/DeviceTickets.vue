7c84e98a877b6633223f8c7582d61de22b8f1675
Evren Ceyhan <evren@ceyhan.io>
2023-03-31 14:08:56 +0200

    device history and tickets moved to partials

---
+++ resources/js/Pages/Devices/Partials/DeviceTickets.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Card from "@/Components/Card.vue";
+import TicketList from "@/Pages/Tickets/Partials/TicketList.vue";
+
+const props = defineProps({
+  device: Object,
+  tickets: Array,
+});
+</script>
+
+<template>
+  <Card flush>
+    <template #header>
+      <h5>
+        Tickets
+        <span class="ml-1 opacity-50">
+          {{ device.closed_tickets_count }}/
+          {{ device.total_tickets_count }}
+        </span>
+      </h5>
+    </template>
+
+    <TicketList :tickets="device.tickets" />
+  </Card>
+</template>
--- resources/js/Pages/Devices/Show.vue
+++ resources/js/Pages/Devices/Show.vue
@@ -1,7 +1,5 @@
 <script setup>
-import { formatDate } from "@/Shared/utils";
 import PageLayout from "@/Layouts/PageLayout.vue";
-import Card from "@/Components/Card.vue";
 import PrimaryButton from "@/Components/Button/PrimaryButton.vue";
 import SecondaryButton from "@/Components/Button/SecondaryButton.vue";
 import DangerButton from "@/Components/Button/DangerButton.vue";
@@ -9,9 +7,9 @@ import Dropdown from "@/Components/Menu/Dropdown.vue";
 import DropdownItem from "@/Components/Menu/DropdownItem.vue";
 import DropdownDivider from "@/Components/Menu/DropdownDivider.vue";
 import DropdownHeader from "@/Components/Menu/DropdownHeader.vue";
-import TicketList from "@/Pages/Tickets/Partials/TicketList.vue";
 import DeviceCard from "./Partials/DeviceCard.vue";
-import DeviceBadge from "./Partials/DeviceBadge.vue";
+import DeviceTickets from "./Partials/DeviceTickets.vue";
+import DeviceHistory from "./Partials/DeviceHistory.vue";
 
 const props = defineProps({
   device: Object,
@@ -139,41 +137,9 @@ const statusActions = [
     </template>
 
     <template #content>
-      <Card flush>
-        <template #header>
-          <h5>
-            Tickets
-            <span class="ml-1 opacity-50">
-              {{ device.closed_tickets_count }}/
-              {{ device.total_tickets_count }}
-            </span>
-          </h5>
-        </template>
-        <TicketList :tickets="device.tickets" />
-      </Card>
+      <DeviceTickets :device="device" :tickets="device.tickets" />
 
-      <!-- <Card label="History" class="!bg-opacity-25">  -->
-      <h3 class="text-xl font-semibold dark:text-white pt-4">History</h3>
-      <ol class="relative border-l border-gray-50 dark:border-gray-700 space-y-10">
-        <li v-for="log in device.logs" :key="log.id" class="ml-4">
-          <div
-            class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"
-          ></div>
-          <time
-            class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-          >
-            {{ formatDate(log.created_at, true) }}
-          </time>
-          <p
-            class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400 space-x-2"
-          >
-            <span class="text-black dark:text-gray-200">{{ log.user.name }}</span>
-            <span>changed status to</span>
-            <DeviceBadge :status="log.status" class="dark:!bg-black/25" />
-          </p>
-        </li>
-      </ol>
-      <!-- </Card> -->
+      <DeviceHistory :logs="device.logs" />
     </template>
   </PageLayout>
 </template>
