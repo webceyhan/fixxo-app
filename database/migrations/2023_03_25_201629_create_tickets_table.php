36b95065534f09e2d3afba1d93bbe6ede64b2c27
Evren Ceyhan <evren@ceyhan.io>
2023-04-06 17:12:13 +0200

    migration column definition order refactored

--- database/migrations/2023_03_25_201629_create_tickets_table.php
+++ database/migrations/2023_03_25_201629_create_tickets_table.php
@@ -15,24 +15,20 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            // TODO: maybe add customer_id field istead of through device?
             $table->foreignId('device_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('description');
             $table->string('note')->nullable();
+            $table->enum('status', TicketStatus::values())->default(TicketStatus::NEW->value);
+            $table->timestamps();
+
             // aggregate fields
-            // TODO: maybe move to separate table?
             $table->decimal('balance')->default(0);
-            // TODO: add total task/order cost fields
-            // TODO: add total paid field
             $table->integer('completed_tasks_count')->default(0);
             $table->integer('total_tasks_count')->default(0);
             $table->integer('received_orders_count')->default(0);
             $table->integer('total_orders_count')->default(0);
-            // ----------------
-            $table->enum('status', TicketStatus::values())->default(TicketStatus::NEW->value);
-            $table->timestamps();
 
             // index definitions
             $table->fullText(Ticket::fullTextColumns());
