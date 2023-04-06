e3d0c1f793b5e596950d338963951aa49d0835fd
Evren Ceyhan <evren@ceyhan.io>
2023-04-06 16:11:48 +0200

    customer table updated with aggragate fields

--- resources/js/Pages/Customers/Partials/CustomerTable.vue
+++ resources/js/Pages/Customers/Partials/CustomerTable.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { formatDate } from "@/Shared/utils";
+import { formatDate, formatMoney } from "@/Shared/utils";
 import Avatar from "@/Components/Avatar.vue";
 import Table from "@/Components/Table/Table.vue";
 import TableRow from "@/Components/Table/TableRow.vue";
@@ -28,18 +28,24 @@ defineProps({
 
       <TableData :value="customer.name" :label="customer.email" label-class="xl:hidden" />
 
-      <TableData class="max-xl:hidden" :label="customer.email" :value="customer.phone" />
+      <TableData class="max-2xl:hidden" :label="customer.email" :value="customer.phone" />
 
       <TableData
-        class="max-xl:hidden text-end"
+        class="max-lg:hidden text-end"
         label="Devices"
         :value="customer.devices_count"
       />
 
+      <TableData class="max-xl:hidden text-end" label="Tickets">
+        <template #value>
+          {{ customer.closed_tickets_count }}/{{ customer.total_tickets_count }}
+        </template>
+      </TableData>
+
       <TableData
-        class="max-xl:hidden text-end"
-        label="Tickets"
-        :value="customer.tickets_count"
+        label="Balance"
+        class="max-xl:hidden text-end whitespace-nowrap"
+        :value="formatMoney(customer.balance)"
       />
 
       <TableData class="max-lg:hidden text-end">
