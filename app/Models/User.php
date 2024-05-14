1fa0a3a92a1c1620bc83556885f1578d8a6cb158
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-05-15 00:07:36 +0200

    Remove obsolete api token trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,11 +14,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, Searchable, HasSince;
+    use  HasFactory, Notifiable, Searchable, HasSince;
 
     /**
      * The attributes that are mass assignable.
