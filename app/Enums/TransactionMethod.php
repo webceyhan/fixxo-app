c98cb7118b6c05c01ac7ad0f1488b05927e816d2
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-05-16 23:33:57 +0200

    Refactor transaction method enum

--- app/Enums/TransactionMethod.php
+++ app/Enums/TransactionMethod.php
@@ -8,7 +8,19 @@ enum TransactionMethod: string
 {
     use HasValues;
 
+    /**
+     * Represents transactions via cash.
+     * @default
+     */
     case Cash = 'cash';
+
+    /**
+     * Represents transactions via credit or debit cards.
+     */
     case Card = 'card';
+
+    /**
+     * Represents transaction via online payment systems.
+     */
     case Online = 'online';
 }
