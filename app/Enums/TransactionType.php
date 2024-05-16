387fc07247071bb7bee01aabf7eb8020e61464ea
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-05-16 23:38:33 +0200

    Refactor transaction type enum

--- app/Enums/TransactionType.php
+++ app/Enums/TransactionType.php
@@ -8,8 +8,24 @@ enum TransactionType: string
 {
     use HasValues;
 
+    /**
+     * Represents a payment transaction.
+     * @default
+     */
     case Payment = 'payment';
+
+    /**
+     * Represents a discount transaction.
+     */
     case Discount = 'discount';
+
+    /**
+     * Represents a warranty reimbursement transaction.
+     */
     case Warranty = 'warranty';
+
+    /**
+     * Represents a refund transaction.
+     */
     case Refund = 'refund';
 }
