f4c6f9a16f991b98fb8ab91e92de9a710d74e17b
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-05-18 23:40:43 +0200

    Refactor searchable concern

--- app/Models/Concerns/Searchable.php
+++ app/Models/Concerns/Searchable.php
@@ -4,6 +4,9 @@ namespace App\Models\Concerns;
 
 use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * @method static Builder|static search(?string $keyword)
+ */
 trait Searchable
 {
     /**
@@ -14,27 +17,27 @@ trait Searchable
     // protected $searchable = [];
 
     /**
-     * Scope a query to only include records matching
-     * given keyword against searchable model attributes.
-     */
-    public function scopeSearch(Builder $query, ?string $keyword = null): void
-    {
-        // skip if keyword is empty
-        if (empty($keyword)) return;
-
-        // append * wildcard to each word to do fuzzy search
-        $keyword = addslashes(implode('* ', explode(' ', $keyword)) . '*');
-
-        // create full-text search in boolean mode for searchable columns
-        $query->whereFullText($this->searchable ?? [], $keyword, ['mode' => 'boolean']);
-    }
-
-    /**
      * Get the full-text index columns.
      * This is used to create the full-text index in the migration.
      */
     public static function fullTextColumns(): array
     {
         return (new static)->searchable ?? [];
+    }
+
+    // SCOPES //////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Scope a query to only include models matching given keyword.
+     */
+    public function scopeSearch(Builder $query, ?string $keyword): void
+    {
+        if (empty($keyword)) {
+            return;
+        }
+
+        $fuzzyKeyword = addslashes(implode('* ', explode(' ', $keyword)) . '*');
+
+        $query->whereFullText($this->searchable, $fuzzyKeyword, ['mode' => 'boolean']);
     }
 }
