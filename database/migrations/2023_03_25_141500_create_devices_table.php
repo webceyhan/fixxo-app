fc88f20a37251985df97e195a324500bc616b360
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-05-18 19:17:14 +0200

    Remove user_id column from devices table

--- database/migrations/2023_03_25_141500_create_devices_table.php
+++ database/migrations/2023_03_25_141500_create_devices_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('brand')->nullable();
             $table->string('type')->nullable();
--- database/seeders/DeviceSeeder.php
+++ database/seeders/DeviceSeeder.php
@@ -19,11 +19,12 @@ class DeviceSeeder extends Seeder
 
         Customer::all()->each(function ($customer) use ($users) {
 
+            auth()->login($users->random(1)->first());
+
             $amount = rand(1, 2);
 
             Device::factory($amount)->create([
                 'customer_id' => fn () => $customer->id,
-                'user_id' => fn () => $users->random(1)->first(),
                 // create date must be later than customer creation
                 'created_at' => fn () => fake()->dateTimeBetween($customer->created_at),
             ]);
