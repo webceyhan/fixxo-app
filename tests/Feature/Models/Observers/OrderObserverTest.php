3583b17e75a109ccb61847366f231d4b83c1802d
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-09-29 23:04:02 +0200

    Add model observer feature tests

---
+++ tests/Feature/Models/Observers/OrderObserverTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Enums\OrderStatus;
+use App\Models\Order;
+use App\Models\Ticket;
+use App\Models\User;
+
+beforeEach(function () {
+    $this->actingAs(User::factory()->create());
+});
+
+it('can update ticket balance on all events', function () {
+    $ticket = Ticket::factory()->create();
+    $order = Order::factory()->forTicket($ticket)->create();
+
+    // ignore cancelled orders
+    Order::factory()->forTicket($ticket)->cancelled()->create();
+
+    $ticket->refresh();
+
+    expect($ticket->balance)->toBe($order->cost);
+
+    // update order cost
+    $order->update(['cost' => 100.0]);
+    $ticket->refresh();
+
+    expect($ticket->balance)->toBe(100.0);
+
+    // delete order
+    $order->delete();
+    $ticket->refresh();
+
+    expect($ticket->balance)->toBe(0.0);
+});
+
+it('can update ticket order counters on all events', function () {
+    $ticket = Ticket::factory()->create();
+    $order = Order::factory()->forTicket($ticket)->create();
+    Order::factory()->forTicket($ticket)->cancelled()->create();
+
+    $ticket->refresh();
+
+    expect($ticket->total_orders_count)->toBe(2);
+    expect($ticket->completed_orders_count)->toBe(1);
+
+    // update order status
+    $order->update(['status' => OrderStatus::Received]);
+    $ticket->refresh();
+
+    expect($ticket->total_orders_count)->toBe(2);
+    expect($ticket->completed_orders_count)->toBe(2);
+
+    // delete order
+    $order->delete();
+    $ticket->refresh();
+
+    expect($ticket->total_orders_count)->toBe(1);
+    expect($ticket->completed_orders_count)->toBe(1);
+});
