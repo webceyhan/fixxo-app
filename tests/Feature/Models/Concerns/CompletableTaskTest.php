5e495864b2038c3c6adf3680a14e1347aa6c1360
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-09-29 23:39:12 +0200

    Add completable concern feature tests for device, ticket, task, order models

---
+++ tests/Feature/Models/Concerns/CompletableTaskTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\TaskStatus;
+use App\Models\Task;
+use App\Models\User;
+
+beforeEach(function () {
+    $this->actingAs(User::factory()->create());
+});
+
+it('can determine if task is completed', function (TaskStatus $status) {
+    $task = Task::factory()->ofStatus($status)->create();
+
+    expect($task->isCompleted())->toBeTrue();
+})->with(TaskStatus::completedCases());
+
+describe('scopes', function () {
+    beforeEach(function () {
+        collect(TaskStatus::cases())->each(function (TaskStatus $status) {
+            Task::factory()->ofStatus($status)->create();
+        });
+    });
+
+    it('can filter tasks by pending scope', function () {
+        expect(Task::pending()->count())->toBe(1);
+        expect(Task::pending()->first()->isCompleted())->toBeFalse();
+    });
+
+    it('can filter tasks by completed scope', function () {
+        expect(Task::completed()->count())->toBe(2);
+        expect(Task::completed()->first()->isCompleted())->toBeTrue();
+    });
+});
