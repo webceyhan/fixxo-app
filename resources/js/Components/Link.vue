7c122c3d94e8263199ae6286fd86fbbde50ad4bd
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-10-22 14:08:36 +0200

    Migrate link component

--- resources/js/Components/Link.vue
+++ resources/js/Components/Link.vue
@@ -11,24 +11,18 @@ const props = defineProps({
 </script>
 
 <template>
-    <component
-        :is="native ? 'a' : Link"
-        :class="{
-            // text
-            'text-sm font-medium': small,
-
-            // primary (default)
-            'text-indigo-500 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-200':
-                !outline && !secondary,
-
-            // secondary
-            'text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100':
-                !outline && secondary,
-
-            // outline
-            'border-b border-dashed border-gray-500': outline,
-        }"
-    >
-        <slot> {{ label }} </slot>
-    </component>
+  <component
+    :is="native ? 'a' : Link"
+    :class="[
+      'link link-hover',
+      {
+        'link-primary': !secondary && !outline,
+        'link-secondary': secondary && !outline,
+        'border-b border-dashed border-neutral-content/30': outline,
+        'text-xm font-medium': small,
+      },
+    ]"
+  >
+    <slot> {{ label }} </slot>
+  </component>
 </template>
