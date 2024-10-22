bbd991dad7685c86813bc3a8e53bf598dfbaeb3e
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-10-22 19:26:39 +0200

    Migrate table components

--- resources/js/Components/Table/TableRow.vue
+++ resources/js/Components/Table/TableRow.vue
@@ -1,19 +1,19 @@
-<script setup>
+<script setup lang="ts">
 import { router } from "@inertiajs/vue3";
 import Icon from "@/Components/Icon.vue";
 import TableData from "@/Components/Table/TableData.vue";
 
-const props = defineProps({
-  href: String,
-});
+const props = defineProps<{
+  href?: string;
+}>();
 
-function onClick(e) {
+const visit = () => {
   props.href && router.visit(props.href);
-}
+};
 </script>
 
 <template>
-  <tr class="hover border-b-0" @click="onClick">
+  <tr class="hover border-b-0" @click="visit">
     <slot />
 
     <TableData v-if="href" class="w-8 pl-0 text-end">
