3fb7e9840c962a31d1235239a0ce728375e1cacb
Evren Ceyhan <evren.ceyhan@gmail.com>
2024-10-22 19:29:59 +0200

    Migrate alert component

--- resources/js/Components/Alert.vue
+++ resources/js/Components/Alert.vue
@@ -1,14 +1,15 @@
-<script setup>
+<script setup lang="ts">
 import Icon from "@/Components/Icon.vue";
 
-defineProps({
-  message: String,
-});
+defineProps<{
+  message?: string;
+}>();
 </script>
 
 <template>
   <div role="alert" class="alert">
     <icon name="info-circle" class="text-2xl text-primary" />
+
     <slot> {{ message }}</slot>
   </div>
 </template>
